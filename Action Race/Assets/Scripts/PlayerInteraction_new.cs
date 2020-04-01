c009821f65d48ec552265ee8b384d864f1a49a39
Tomasz Kula <tomkul.priv@gmail.com>
2020-04-02 01:18:49 +0200

    antennas synchro

---
+++ Action Race/Assets/Scripts/PlayerInteraction_new.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction_new : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (!col.CompareTag("Antenna")) return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("Interact");
+
+            AntennaController ac = col.GetComponent<AntennaController>();
+            ac.ProgramAntenna();
+        }
+    }
+}
