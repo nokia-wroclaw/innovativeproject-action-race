707b9603858f8b7e23e11b29903a01761c75f1a4
Tomasz Kula <tomkul.priv@gmail.com>
2020-04-02 02:27:27 +0200

    Agata's scene works again!

---
+++ Action Race/Assets/Scripts/Network/JoinRoomManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+
+public class JoinRoomManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] RoomListInfo roomListInfo;
+
+    List<RoomInfo> roomList = new List<RoomInfo>();
+
+    public void JoinRoom(Room roomPanel)
+    {
+        Debug.Log("JoinRoom() " + roomPanel.RoomName);
+        PhotonNetwork.JoinRoom(roomPanel.RoomName);
+    }
+
+    public void Refresh()
+    {
+        roomListInfo.ClearRoomList();
+
+        foreach (RoomInfo roomInfo in roomList)
+        {
+            string roomPassword = roomInfo.CustomProperties["Password"].ToString();
+            if (!roomListInfo.ShowPrivate && !string.IsNullOrEmpty(roomPassword.Trim())) continue;
+
+            string roomName = roomInfo.Name;
+            string roomOwner = roomInfo.CustomProperties["Owner"].ToString();
+            string textFilter = roomListInfo.TextFilter.Trim();
+            if (!roomName.ToLower().Contains(textFilter.ToLower()) && !roomOwner.ToLower().Contains(textFilter.ToLower())) continue;
+
+            string roomMode = "";
+
+            int roomPlayers = roomInfo.PlayerCount;
+            int roomMaxPlayers = roomInfo.MaxPlayers;
+            if (!roomListInfo.ShowFull && roomPlayers == roomMaxPlayers) continue;
+
+            roomListInfo.AddRoom(roomPassword, roomName, roomOwner, roomMode, roomPlayers, roomMaxPlayers);
+        }
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        Debug.Log("OnRoomListUpdate");
+        this.roomList = roomList;
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRoomFailed");
+    }
+}
---
+++ Action Race/Assets/Scripts/Network/QuickPlayManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class QuickPlayManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] UIManager uiManager;
+
+    public void QuickPlay()
+    {
+        Debug.Log("QuickPlay");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("No room to join, create your own");
+
+        uiManager.switchFrom_QuickPlayPanel_to_CreateRoomPanel();
+    }
+}
