5ac55997e78c69fdb45a0e654a8518132f5c84d5
agata987 <37309188+agata987@users.noreply.github.com>
2020-04-06 23:43:09 +0200

    hud added

---
+++ Action Race/Assets/Scripts/GUI/HUD/HUD_manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HUD_manager : MonoBehaviour
+{
+    public GameObject Infopanel;
+
+    public void exitToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene",LoadSceneMode.Single);    
+    }
+
+    public void openInfoPanel()
+    {
+        if (Infopanel != null)
+        {
+            Infopanel.SetActive(true);
+        }
+        else
+        {
+            System.Console.WriteLine("panel to open is null");
+        }
+    }
+
+    public void closeInfoPanel()
+    {
+        if (Infopanel != null)
+        {
+            Infopanel.SetActive(false);
+        }
+        else
+        {
+            System.Console.WriteLine("panel to close is null");
+        }
+    }
+}
