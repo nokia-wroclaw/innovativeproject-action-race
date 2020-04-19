2c4d48cee08a24acbf321a94df03514c034002db
agata987 <37309188+agata987@users.noreply.github.com>
2020-04-19 23:37:41 +0200

    won/lost panels added

---
+++ Action Race/Assets/Scripts/HUD/SequenceAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceAnimator : MonoBehaviour
+{
+    List<Animator> animators;
+    public float waitBetween = 0.15F;
+    public float waitEnd = 0.2F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animators = new List<Animator>(GetComponentsInChildren<Animator>());
+        StartCoroutine(doAnimation());
+    }
+
+    IEnumerator doAnimation()
+    {
+        while(true)
+        {
+            foreach (var animator in animators)
+            {
+                animator.SetTrigger("DoAnimation");
+                yield return new WaitForSeconds(waitBetween);
+            }
+
+            yield return new WaitForSeconds(waitEnd);
+        }
+    }
+}
